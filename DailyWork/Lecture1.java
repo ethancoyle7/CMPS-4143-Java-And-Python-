c1e24607725bec8d6c543bd25bcb6af87d5c63e9
Anon-583 <77364000+ethancoyle7@users.noreply.github.com>
2021-08-30 17:43:24 -0500

    Create Lecture1.java

---
+++ DailyWork/Lecture1.java
@@ -0,0 +1,25 @@
+/**
+ * this is the first program day
+ * version SE9
+ * author - Ethan Coyle
+ * Class -4143
+ */
+class HelloWorld // camelcase class name
+ {
+   //main class inside of the hello world class
+    public static void main(String[] args) 
+    //main class inside of the hello world class
+    {
+        System.out.println("Hello, World!"); 
+        //print out on new line to the system console
+        //the string inside is the method
+
+        //declarations of a arrays in java
+        //int rolls[]= new int [10];
+        //int[] a= new int [5];
+        //initialization of an array
+        //int[] b =new int [] {2,3,4,5,6};
+
+
+    }   
+}
