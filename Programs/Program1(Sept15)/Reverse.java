6011218894bdabcb0312b37000cf2570dbc3f1a2
Anon-583 <77364000+ethancoyle7@users.noreply.github.com>
2021-09-01 17:16:07 -0500

    Create Reverse.java

---
+++ Programs/Program1(Sept15)/Reverse.java
@@ -0,0 +1,11 @@
+
+public class Reverse 
+{
+    
+    public static void main(String args[])
+    {
+     String s="Before trying to do any of the steps below, you should read the article through at least once for basic understanding. Then go back and review, following these steps.He ran his machine up to the stone porch and ascending the steps rang the door bell. They directed their steps toward the sea, which was lit up by the rising moon. She breathed a sigh of relief, and her light steps fell gradually into the measure of his. This was fully four feet under water and the lower story of the place was two steps lower down.";
+     String reverse = new StringBuffer(s).reverse().toString();
+     System.out.println(reverse);
+    }
+}
