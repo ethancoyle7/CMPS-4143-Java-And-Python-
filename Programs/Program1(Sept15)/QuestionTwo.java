b387c22c2a917c8c7044f66569dae46281c903fe
Anon-583 <77364000+ethancoyle7@users.noreply.github.com>
2021-09-02 18:25:39 -0500

    Create QuestionTwo.java

---
+++ Programs/Program1(Sept15)/QuestionTwo.java
@@ -0,0 +1,73 @@
+//##########################################################################################//
+//                                                                                          //
+//   Author-           Ethan Coyle                                                          //
+//   Instructor/Class- Dr.Saikat-CMPS 4143 Java/Pyhton                                      //
+//   Due Date-         Septermeber15,2021                                                   //
+//   What it does-                                                                          //
+//                                                                                          //
+//   This File contains the answer to the Question #2 on Program1                           //
+//   In this program, will be copying over the values from one array to another             //
+//   will also be comparing the values inside and arranging in descending order             //
+//   Also, will be comparing to see the avg in the original array and then printing it out  //
+//                                                                                          //
+//##########################################################################################//
+
+
+public class QuestionTwo
+ {
+    public static void main(String[] args)
+
+    {
+        int DescendingArray=0;//create a DescendingArrayorary holder start at 0 Used in Descending
+        int Array_X[] = { 10,100,40,28,98,37,12,63 };
+        int Array_Y[] = new int[Array_X.length];
+        Array_Y = Array_X;
+        Array_Y[0]++;
+
+        System.out.println("The Contents of Original Array Are : ");
+
+        for (int i = 0; i < Array_X.length; i++)
+            System.out.print(Array_X[i] + " ");
+            
+        System.out.println("\n\nThe Contents Array Y are :  ");
+        
+        //for loop to print out the values of the copied array
+        for (int i = 0; i < Array_Y.length; i++)
+            System.out.print(Array_Y[i] + " ");
+        
+        //below is going to iterate through in descending order of the original array
+        int n= Array_X.length;//iterate through the length of array
+        //comparative nested for loop to iterate and compare values
+        for (int i = 0; i < n; i++) 
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                //test to see ivalue of i is less than next
+                if (Array_X[i] < Array_X[j]) 
+                {
+                    //store values into DescendingArray rel
+                    DescendingArray = Array_X[i];
+                    Array_X[i] = Array_X[j];
+                    Array_X[j] = DescendingArray;
+                    //assign the new values to original in descending order
+                }
+            
+            }
+            
+        }
+            //console print the array in the descending order
+            System.out.print("\n\nIn Descending Order:\n");
+            
+            for (int i = 0; i < n - 1; i++) 
+            {
+            //formatting for array
+            System.out.print(Array_X[i] + ",");
+            
+            }
+    //print out the descending order array
+    //original array now ordered in descending value order
+    System.out.print(Array_X[n - 1]+"\n\n");
+            
+     }      
+
+}
