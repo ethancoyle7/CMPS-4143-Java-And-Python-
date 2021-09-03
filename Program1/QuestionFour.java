4681aba838b22b7ff7445becaceb2eb978e5b447
Anon-583 <77364000+ethancoyle7@users.noreply.github.com>
2021-09-03 15:06:14 -0500

    Create QuestionFour.java

---
+++ Program1/QuestionFour.java
@@ -0,0 +1,64 @@
+//Ethan Coyle
+//Dr.Saikat
+//Program 1 Problem 4
+class Reverse 
+{
+    static void reverse(char str[],int Beginning,int Ending) 
+    {
+     // First create StringLINE variable to hold the string
+        char StringLINE;  
+        while (Beginning <= Ending)
+        {
+            // Inside here, we are swapping around the string
+            StringLINE = str[Beginning];
+            str[Beginning] = str[Ending];
+            str[Ending] = StringLINE;
+            Beginning++;
+            Ending--;
+        }
+
+    }
+
+// Next we must create function to reverse the words
+
+    static char[] StringSwap(char []s)
+    {
+    int Beginning = 0;//initialize the starting point to 0
+    
+    for (int Ending = 0; Ending < s.length; Ending++)
+    {
+        if (s[Ending] == ' ')
+        {
+        
+        reverse(s, Beginning, Ending);
+        Beginning = Ending + 1;
+
+        }
+
+    }
+
+    // Reverse the last word
+
+    reverse(s, Beginning, s.length - 1);
+
+    // putting the whole string in reverse
+    reverse(s, 0, s.length - 1);
+    //retruning the string that is flipped
+    return s;
+    }
+
+//Now that we have both the read and flip functions
+//it is time to do driver code to read in the dtring
+//go to appropriate functions and display the flipped 
+//string that was read in
+
+    public static void main(String[] args)
+
+    {
+        String s = "this is my first programming  assignment";
+        char []p = StringSwap(s.toCharArray());
+        System.out.print(p);
+
+    }
+
+}//end of class reverse
