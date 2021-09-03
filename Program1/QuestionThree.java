dcc9930297af57f30b93ac5844d8953f8cc19b6a
Anon-583 <77364000+ethancoyle7@users.noreply.github.com>
2021-09-03 15:05:36 -0500

    Create QuestionThree.java

---
+++ Program1/QuestionThree.java
@@ -0,0 +1,88 @@
+//##########################################################################################//
+//                                                                                          //
+//   Author-           Ethan Coyle                                                          //
+//   Instructor/Class- Dr.Saikat-CMPS 4143 Java/Pyhton                                      //
+//   Due Date-         Septermeber15,2021                                                   //
+//   What it does-                                                                          //
+//                                                                                          //
+//   This File contains the answer to the Question #3 on Program1                           //
+//   This Program,we are given a large string, and we are being asked to run through the    //
+//   The string and find the % most occured words inside of the string, In doing, We will   //
+//   Store the occured words in an array and then run through some tests and then output to //
+//   the screen the top 5 most occured Thus satisfying the given question                   //
+//                                                                                          //
+//##########################################################################################//
+public class HighFrequency //same name as the given java program
+{
+            public static void main(String[] args)//our main driver
+         {
+            ///First list the given string in the given example
+            String S = "Before trying to do any of the steps below, you should read the article " +
+                    "through at least once for basic understanding. Then go back and review, following these steps. " +
+                    "He ran his machine up to the stone porch and ascending the steps rang the door bell. They " +
+                    "directed their steps toward the sea, which was lit up by the rising moon. She breathed a sigh of " +
+                    "relief, and her light steps fell gradually into the measure of his. This was fully four feet under "+
+                    "water and the lower story of the place was two steps lower down.";
+
+            //for formatting purposes and visual presentation, print out the string
+            System.out.println(S);
+    
+            String[] words = S.split("\\s");
+
+             //next, we are going to look and find the most frequent words given
+            String[] FrequentWords = new String[words.length];
+            int[] frequency = new int[words.length];
+
+            //dyanmicall allocate new mem for frequency
+            //initialize a counter variable to store the count
+            int OccuranceCount = 0;
+
+            //starting a for loop to run through the string a run through frequency 
+            
+            for (int i = 0; i < words.length; i++) 
+            {
+                boolean exists = false;// need to check booleans to see if frequency does exist
+                for (int j = 0; j < OccuranceCount; j++) 
+                {
+                    if (words[i].replaceAll("[.,;]", "").equalsIgnoreCase(FrequentWords[j])) 
+                    {
+                        frequency[j] += 1;
+                        exists = true;
+    
+                    }
+                }
+                if (!exists)// if the boolean is true, then add to counter 
+                {
+                    FrequentWords[OccuranceCount] = words[i].replaceAll("[.,;]", "");
+                    frequency[OccuranceCount] = 1;
+                    OccuranceCount += 1;
+                }
+            }
+    
+            //Next for loop sorts the arrays of occurancies
+    
+            for (int i = 0; i < OccuranceCount; i++) 
+            {
+                for (int j = 0; j < OccuranceCount - i - 1; j++) 
+                {
+                    if (frequency[j] < frequency[j + 1]) 
+                    {
+                        //temporary holding plave for frequency
+                        int temp = frequency[j];
+                        frequency[j] = frequency[j + 1];
+                        frequency[j + 1] = temp;
+                        String tempS = FrequentWords[j];
+                        FrequentWords[j] = FrequentWords[j + 1];
+                        FrequentWords[j + 1] = tempS;
+                    }
+                }
+            }
+            // Lastly, we systematically print out the % most frequent words that are found
+            System.out.println("The Top % Most Frequent Words Are -");
+            for (int i = 0; i < 5; i++)
+             {
+                 //loop through for loop print out the most frequet that is store in the array
+                System.out.println(FrequentWords[i] + " - This  appeared " + frequency[i] + " times.");
+            }
+        }
+}//end of process and end of program
