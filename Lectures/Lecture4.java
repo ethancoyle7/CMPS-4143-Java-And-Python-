4d7591d61ac4394fcc2bd2f38a5d2d25d68bb90f
ethancoyle7 <77364000+ethancoyle7@users.noreply.github.com>
2021-09-08 15:11:29 -0500

    finishing Lecture4

--- Lectures/Lecture4.java
+++ Lectures/Lecture4.java
@@ -2,21 +2,31 @@ public class Lecture4
 {
    
         // first initialize instance variable that are private
-        private String PetsName;
-        private int PetsAge;
-        private String PetsBreed;
+        String PetsName;
+        int PetsAge;
+        String PetsBreed;
+        String Color;
 
         // This is the constructor that initializes the pet to default values
-        public Puppy (String PuppyName, int PuppyAge,string PuppyBreed)
+        public Lecture4 (String PuppyName, int PuppyAge,String PuppyBreed, String DogColor)
         {
                 //called upon whenever the puppy class is invoked
                 PetsName=PuppyName;
                 PetsAge=PuppyAge;
                 PetsBreed=PuppyBreed;
-
+                Color=DogColor;
         }
 
         // initialize our accessors and mutators
+        public void setColor(String DogColor)
+        { 
+            Color=DogColor;//setting the color(accesor)
+       }
+       //creation of the accessor called when initialized in the driver of the program
+       public String getColor() 
+       {
+           return Color;//grabbing the color
+       }
         public void setName(String PuppyName)
          { 
              PetsName=PuppyName;
@@ -36,12 +46,12 @@ public class Lecture4
             return PetsAge;
         }
         
-        public void setBreed(int PuppyBreed) 
+        public void setBreed(String PuppyBreed) 
         {
             PetsBreed=PuppyBreed;
         }
         //accesor to the breed string
-        public int getBreed() 
+        public String getBreed() 
         {
             return PetsBreed;
         }
@@ -50,12 +60,15 @@ public class Lecture4
         public static void main(String args[]) //this is our main driver program to print out stuffs
         {
 
-                Puppy Dog1=new Puppy("Bilbo Bsggind",2000,"Dalmation"); // two different puppiers
-                Puppy Dog2=new Puppy("Harry Potter",12,"Hobbit");//second object dog
+                Lecture4 Dog1=new Lecture4(" Bilbo Baggin ",2000," Dalmation"," Orange/Purple"); // two different puppiers
+                Lecture4 Dog2=new Lecture4(" Harry Potter ",12," Hobbit", " Muddy Brown ");//second object dog
 
                 //print out to the console
-                System.out.println(Dog1.getName()+"'s PuppyAge is "+Dog1.getAge() + "PuppyBreed is "+Dog1.getBreed());
-                System.out.println(Dog2.getName()+"'s PuppyAge is "+Dog2.getAge()+ "PuppyBreed is "+Dog2.getBreed());;
+                System.out.println(Dog1.getName()+"'s Age is "+Dog1.getAge() +
+                     "His Breed is a "+Dog1.getBreed() + " His Color is "+Dog1.getColor());
+
+                System.out.println(Dog2.getName()+"'s Age is "+Dog2.getAge()+ 
+                    " His Breed is a  "+Dog2.getBreed()+ " His Color is "+Dog2.getColor());
 
         }
 }
