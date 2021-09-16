b40f0aa9ff5fd710e5942d20277008752d23a29a
ethancoyle7 <77364000+ethancoyle7@users.noreply.github.com>
2021-09-15 23:11:56 -0500

    fromat

--- Program1/QuestionThree.java
+++ Program1/QuestionThree.java
@@ -7,9 +7,9 @@
 //                                                                                          //
 //   This File contains the answer to the Question #3 on Program1                           //
 //   This Program,we are given a large string, and we are being asked to run through the    //
-//   The string and find the % most occured words inside of the string, In doing, We will   //
-//   Store the occured words in an array and then run through some tests and then output to //
-//   the screen the top 5 most occured Thus satisfying the given question                   //
+//   The string and find the % most occured StringWords inside of the string, In doing, We  //
+//   Store the occured StringWords in an array and then run through some tests and then     //
+//   output to the screen the top 5 most occured Thus satisfying the given question         //
 //                                                                                          //
 //##########################################################################################//
 public class QuestionThree //same name as the given java program
@@ -17,22 +17,24 @@ public class QuestionThree //same name as the given java program
             public static void main(String[] args)//our main driver
          {
             ///First list the given string in the given example
-            String S = "Before trying to do any of the steps below, you should read the article " +
-                    "through at least once for basic understanding. Then go back and review, following these steps. " +
-                    "He ran his machine up to the stone porch and ascending the steps rang the door bell. They " +
-                    "directed their steps toward the sea, which was lit up by the rising moon. She breathed a sigh of " +
-                    "relief, and her light steps fell gradually into the measure of his. This was fully four feet under "+
+            String S = "Before trying to do any of the steps below, you should read the article"+
+                    "through at least once for basic understanding. Then go back and review,"+
+                    " following these steps He ran his machine up to the stone porch and ascending"+ 
+                    "the steps rang the door bell. Theydirected their steps toward the sea, which"+
+                    "was lit up by the rising moon. She breathed a sigh of relief, and her light"+
+                    "steps fell gradually into the measure of his. This was fully four feet under "+
                     "water and the lower story of the place was two steps lower down.";
 
             //for formatting purposes and visual presentation, print out the string
-            System.out.println(S);
+            System.out.println("Our Provided String From The Question is "+S+'\n');
     
-            String[] words = S.split("\s");
+            String[] StringWords = S.split("\s");// split the words up so can count occureances
 
-             //next, we are going to look and find the most frequent words given
-             //create two dynamic allocated arrays to store frequent words and their Recurrance
-            String[] FrequentWords = new String[words.length];
-            int[] Recurrance = new int[words.length];
+             //next, we are going to look and find the most frequent StringWords given
+             //create two dynamic allocated arrays to store frequent StringWords and their Recurrance
+
+            String[] WordsFromOurString = new String[StringWords.length];
+            int[] Recurrance = new int[StringWords.length];
 
             //dyanmicall allocate new mem for Recurrance
             //initialize a counter variable to store the count
@@ -40,23 +42,24 @@ public class QuestionThree //same name as the given java program
 
             //starting a for loop to run through the string a run through Recurrance 
             
-            for (int i = 0; i < words.length; i++) 
+            for (int i = 0; i < StringWords.length; i++) 
             {
-                boolean exists = false;// need to check booleans to see if Recurrance does exist
+                boolean exists = false; // assume that a recurrance hasnt happened
                 for (int j = 0; j < OccuranceCount; j++) 
                 {
+                    
                     // cpaital and lower case dont matter if word occurs add to Recurrance
-                    if (words[i].replaceAll("[.,;]", "").equalsIgnoreCase(FrequentWords[j])) 
+                    if (StringWords[i].replaceAll("[.,;]", "").equalsIgnoreCase(WordsFromOurString[j])) 
                     {
                         //if an occurancy does occur add to freguency 
                         Recurrance[j] += 1;
-                        exists = true; // it has reoccured
+                        exists = true;
     
                     }
                 }
                 if (!exists)// if the boolean is true, then add to counter 
                 {
-                    FrequentWords[OccuranceCount] = words[i].replaceAll("[.,;]", "");
+                    WordsFromOurString[OccuranceCount] = StringWords[i].replaceAll("[.,;]", "");
                     Recurrance[OccuranceCount] = 1;
                     OccuranceCount += 1;
                 }
@@ -73,23 +76,24 @@ public class QuestionThree //same name as the given java program
                         //temporary holding plave for Recurrance
                         // while we are going through the occurancy placeholders, we are going to 
                         //  assign the highest values to temp for printing out
-                        int temp = Recurrance[j];
+                        int PlaceHolder = Recurrance[j];
                         Recurrance[j] = Recurrance[j + 1];
-                        Recurrance[j + 1] = temp;
-                        String tempS = FrequentWords[j];
-                        FrequentWords[j] = FrequentWords[j + 1];
-                        FrequentWords[j + 1] = tempS;
+                        Recurrance[j + 1] = PlaceHolder;
+                        //creating placeholder to hold recurrances  into Temporary String
+                        String TemporaryStringS = WordsFromOurString[j];
+                        WordsFromOurString[j] = WordsFromOurString[j + 1];
+                        WordsFromOurString[j + 1] = TemporaryStringS;
                     }
                 }
             }
-            // Lastly, we systematically print out the % most frequent words that are found
-            System.out.println("\nThe Top 5 Most Frequently Occured Words in this string are -");
+            // Lastly, we systematically print out the % most frequent StringWords that are found
+            System.out.println("\nThe Top 5 Most Frequently Occured StringWords in this string are -");
             System.out.println("---------------------------------------------------------------");
             for (int i = 0; i < 5; i++)
              {
                  //loop through for loop print out the most frequet that is store in the array
-                System.out.println("["+ FrequentWords[i] + "] Appearing "+ Recurrance[i] + " Times. ");
+                System.out.println("The Word ["+ WordsFromOurString[i] + "] Appearing "+ Recurrance[i] + " Times. ");
             }
             System.out.println("---------------------------------------------------------------");
-        }
+        }//end of driver
 }//end of process and end of program
