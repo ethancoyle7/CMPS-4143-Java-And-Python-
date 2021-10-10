ea4e18f69d0347c1eda4c2114ba6386d26911968
ethancoyle7 <77364000+ethancoyle7@users.noreply.github.com>
2021-10-09 21:42:26 -0500

    delete crappy stuff

--- Lectures/JavaProg/javaconvert/DefineConstants.java
+++
@@ -1,7 +0,0 @@
-package JavaProg.javaconvert;
-
-final class DefineConstants
-{
-	public static final int MEGAPLIER = 15; // balls numbered 1-15
-	public static final int REGULARBALLS = 70; // there are 70 balls
-}
