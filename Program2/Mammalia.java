40d38fd67007c032ae4ae858afc5e8d132a9b960
ethancoyle7 <77364000+ethancoyle7@users.noreply.github.com>
2021-10-12 18:28:12 -0500

    formatting

--- Program2/Mammalia.java
+++ Program2/Mammalia.java
@@ -60,13 +60,13 @@ class Mammilia
     {
         
         //returning the set variables and values per instance of each case of derive mammalia
-        return ("I Have  " + Legs + " Leg/s\n"+ 
-        "I Have a Tail? " + HasTail + "\n"+
-         "Do I have a Brain?  " + HasABrain + "\n"+
-          " I have? " + NumNeurons + " Ammount Of Neurons \n"
-        + "My Total Level Percentage of Precision is  " + LevelOfPrecision + "% Im a little Smart \n"+
-         "I was Born in the Year? " + DateOfBirth + "\n"+
-          "And My Favorite Food is ? " + FavoriteFood +  " Isnt that Yummy? \n");
+        return ("I Have                    " + Legs + " Leg/s\n"+ 
+         "I Have a Tail?            " + HasTail + "\n"+
+         "Do I have a Brain         " + HasABrain + "\n"+
+         "I have                    " + NumNeurons + " Neurons \n"
+        + "My Precision Level is     " + LevelOfPrecision + "% (Im a little Smart) \n"+
+         "I was Born in the Year    " + DateOfBirth + "\n"+
+         "And My Favorite Food is   " + FavoriteFood +  " (Isnt that Yummy? )\n");
     }
 }
 
@@ -661,23 +661,23 @@ public class Mammalia
 
         // problem one says to print out five animals from the families
         Perissodactyla ButterBean = new Perissodactyla(80, true, true, 12, 1.2, 1954, "Butter  and Peanuts");
-        System.out.println(" I am a Perissodactyla\n"+ButterBean.Attributes());
-
+        System.out.println(" I am a Perissodactyla\n"+"---------------------------------\n"+ButterBean.Attributes());
+                        
         // animal 2
         Mammilia BubbleMan = new Theria(2, true, true, 11, 88.3, 1332, "Air and Little Hobbits");
-        System.out.println(" I am a Theria\n"+BubbleMan.Attributes());
+        System.out.println(" I am a Theria\n"+"---------------------------------\n"+BubbleMan.Attributes());
 
         //mammal3
         Mammilia Boreoeutheria1 = new Boreoeutheria(19, true, true, 200, 99.9, 1283, "Celery and Potatoes");
-        System.out.println(" I am a Boreoeutheria\n"+ Boreoeutheria1.Attributes());
+        System.out.println(" I am a Boreoeutheria\n"+"---------------------------------\n" +Boreoeutheria1.Attributes());
 
         //mammal 4 creating
-        Pholidota Laurasiatheria1 = new Pholidota(80, true, true, 9, 40.3, 1542, "Nothing");
-        System.out.println(" I am a Pholidota\n"+Laurasiatheria1.Attributes());
+        Pholidota Pholidota1 = new Pholidota(80, true, true, 9, 40.3, 1542, "Nothing");
+        System.out.println(" I am a Pholidota\n"+"---------------------------------\n"+Pholidota1.Attributes());
 
         //mammalia 5 creating
         Mammilia Placentalia1 = new Placentalia(5, true, true, 800, 10.0, 2030, "McDonals and Ketchup Sauce");
-        System.out.println("I am a Placentia\n"+Placentalia1.Attributes());
+        System.out.println("I am a Placentia\n"+"---------------------------------\n"+Placentalia1.Attributes());
 
 
 
@@ -688,104 +688,104 @@ public class Mammalia
 
         // create five instances of new mammals that are derived from the different levels
         Marsupialia Marsup1=new Marsupialia(4, true, true, 80, 90.0, 1287, "Pork and Prime Rib");
-        System.out.println("I am a Marsupialia\n"+Marsup1.Attributes());
+        System.out.println("I am a Marsupialia\n"+"---------------------------------\n"+Marsup1.Attributes());
         Marsupialia Marsup2=new Marsupialia(0, true, true, 0, 0.0, 1500, "Bugs and Worms");
-        System.out.println("I am a Marsupialia\n"+Marsup2.Attributes());
+        System.out.println("I am a Marsupialia\n"+"---------------------------------\n"+Marsup2.Attributes());
         Marsupialia Marsup3=new Marsupialia(1, true, true, 10, 100, 1568, "Blue Cheese and Crackers");
-        System.out.println("I am a Marsupialia\n"+Marsup3.Attributes());
+        System.out.println("I am a Marsupialia\n"+"---------------------------------\n"+Marsup3.Attributes());
         Marsupialia Marsup4=new Marsupialia(8, true, true, 78, 90.9, 1567, "Rocks");
-        System.out.println("I am a Marsupialia\n"+Marsup4.Attributes());
+        System.out.println("I am a Marsupialia\n"+"---------------------------------\n"+Marsup4.Attributes());
         Marsupialia Marsup5=new Marsupialia(4, true, true, 1, 5.0, 1289, "Spaghetti");
-        System.out.println("I am a Marsupialia\n"+Marsup5.Attributes());
+        System.out.println("I am a Marsupialia\n"+"---------------------------------\n"+Marsup5.Attributes());
 
         Afrotheria Afro1=new Afrotheria(4, true, true, 80, 90.0, 1287, "Beries and Pork Belly");
-        System.out.println("I am a Afrotheria\n"+Afro1.Attributes());
+        System.out.println("I am a Afrotheria\n"+"---------------------------------\n"+Afro1.Attributes());
         Afrotheria Afro2=new Afrotheria(0, true, true, 0, 0.0, 1500, " Fried Caviar");
-        System.out.println("I am a Afrotheria\n"+Afro2.Attributes());
+        System.out.println("I am a Afrotheria\n"+"---------------------------------\n"+Afro2.Attributes());
         Afrotheria Afro3=new Afrotheria(1, true, true, 10, 100, 1568, "Mold");
-        System.out.println("I am a Afrotheria\n"+Afro3.Attributes());
+        System.out.println("I am a Afrotheria\n"+"---------------------------------\n"+Afro3.Attributes());
         Afrotheria Afro4=new Afrotheria(8, true, true, 78, 90.9, 1567, "Ants and Mold Catapillers");
-        System.out.println("I am a Afrotheria\n"+Afro4.Attributes());
+        System.out.println("I am a Afrotheria\n"+"---------------------------------\n"+Afro4.Attributes());
         Afrotheria Afro5=new Afrotheria(4, true, true, 1, 5.0, 1289, "Eggs and Bacon");
-        System.out.println("I am a Afrotheria\n"+Afro5.Attributes());
+        System.out.println("I am a Afrotheria\n"+"---------------------------------\n"+Afro5.Attributes());
 
         Xenarthra Xena1=new Xenarthra(4, true, true, 80, 90.0, 1287, "Banana and More Bananas");
-        System.out.println("I am a Xenarthra\n"+Xena1.Attributes());
+        System.out.println("I am a Xenarthra\n"+"---------------------------------\n"+Xena1.Attributes());
         Xenarthra Xena2=new Xenarthra(0, true, true, 0, 0.0, 1500, "Cherries and Worms");
-        System.out.println("I am a Xenarthra\n"+Xena2.Attributes());
+        System.out.println("I am a Xenarthra\n"+"---------------------------------\n"+Xena2.Attributes());
         Xenarthra Xena3=new Xenarthra(1, true, true, 10, 100, 1568, "Drt and Ants");
-        System.out.println("I am a Xenarthra\n"+Xena3.Attributes());
+        System.out.println("I am a Xenarthra\n"+"---------------------------------\n"+Xena3.Attributes());
         Xenarthra Xena4=new Xenarthra(8, true, true, 78, 90.9, 1567, "Brocolli and Rocks");
-        System.out.println("I am a Xenarthra\n"+Xena4.Attributes());
+        System.out.println("I am a Xenarthra\n"+"---------------------------------\n"+Xena4.Attributes());
         Xenarthra Xena5=new Xenarthra(4, true, true, 1, 5.0, 1289, "Beans and Beans");
-        System.out.println("I am a Xenarthra\n"+Xena5.Attributes());
+        System.out.println("I am a Xenarthra\n"+"---------------------------------\n"+Xena5.Attributes());
 
         Glires Gliry1=new Glires(4, true, true, 80, 90.0, 1287, "Carrotss");
-        System.out.println("I am a Glires\n"+Gliry1.Attributes());
+        System.out.println("I am a Glires\n"+"---------------------------------\n"+Gliry1.Attributes());
         Glires Gliry2=new Glires(0, true, true, 0, 0.0, 1500, "Other Animals");
-        System.out.println("I am a Glires\n"+Gliry2.Attributes());
+        System.out.println("I am a Glires\n"+"---------------------------------\n"+Gliry2.Attributes());
         Glires Gliry3=new Glires(1, true, true, 10, 100, 1568, "Sticks and Dirt");
-        System.out.println("I am a Glires\n"+Gliry3.Attributes());
+        System.out.println("I am a Glires\n"+"---------------------------------\n"+Gliry3.Attributes());
         Glires Gliry4=new Glires(8, true, true, 78, 90.9, 1567, "Brocolli and Rocks");
-        System.out.println("I am a Glires\n"+Gliry4.Attributes());
+        System.out.println("I am a Glires\n"+"---------------------------------\n"+Gliry4.Attributes());
         Glires Gliry5=new Glires(4, true, true, 1, 5.0, 1289, "Beans and Beans");
-        System.out.println("I am a Glires\n"+Gliry5.Attributes());
+        System.out.println("I am a Glires\n"+"---------------------------------\n"+Gliry5.Attributes());
 
         Chiroptera Chiro1=new Chiroptera(4, true, true, 80, 90.0, 1287, "Carrotss");
-        System.out.println("I am a Chiroptera\n"+Chiro1.Attributes());
+        System.out.println("I am a Chiroptera\n"+"---------------------------------\n"+Chiro1.Attributes());
         Chiroptera Chiro2=new Chiroptera(0, true, true, 0, 0.0, 1500, "Other Animals");
-        System.out.println("I am a Chiroptera\n"+Chiro2.Attributes());
+        System.out.println("I am a Chiroptera\n"+"---------------------------------\n"+Chiro2.Attributes());
         Chiroptera Chiro3=new Chiroptera(1, true, true, 10, 100, 1568, "Sticks and Dirt");
-        System.out.println("I am a Chiroptera\n"+Chiro3.Attributes());
+        System.out.println("I am a Chiroptera\n"+"---------------------------------\n"+Chiro3.Attributes());
         Chiroptera Chiro4=new Chiroptera(8, true, true, 78, 90.9, 1567, "Brocolli and Rocks");
-        System.out.println("I am a Chiroptera\n"+Chiro4.Attributes());
+        System.out.println("I am a Chiroptera\n"+"---------------------------------\n"+Chiro4.Attributes());
         Chiroptera Chiro5=new Chiroptera(4, true, true, 1, 5.0, 1289, "Beans and Beans");
-        System.out.println("I am a Chiroptera\n"+Chiro5.Attributes());
+        System.out.println("I am a Chiroptera\n"+"---------------------------------\n"+Chiro5.Attributes());
 
 
         Cetartiodactyla Certa1=new Cetartiodactyla(4, true, true, 80, 90.0, 1287, "Carrotss");
-        System.out.println("I am a Cetartiodactyla\n"+Certa1.Attributes());
+        System.out.println("I am a Cetartiodactyla\n"+"---------------------------------\n"+Certa1.Attributes());
         Cetartiodactyla Certa2=new Cetartiodactyla(0, true, true, 0, 0.0, 1500, "Other Animals");
-        System.out.println("I am a Cetartiodactyla\n"+Certa2.Attributes());
+        System.out.println("I am a Cetartiodactyla\n"+"---------------------------------\n"+Certa2.Attributes());
         Cetartiodactyla Certa3=new Cetartiodactyla(1, true, true, 10, 100, 1568, "Sticks and Dirt");
-        System.out.println("I am a Cetartiodactyla\n"+Certa3.Attributes());
+        System.out.println("I am a Cetartiodactyla\n"+"---------------------------------\n"+Certa3.Attributes());
         Cetartiodactyla Certa4=new Cetartiodactyla(8, true, true, 78, 90.9, 1567, "Brocolli and Rocks");
-        System.out.println("I am a Cetartiodactyla\n"+Certa4.Attributes());
+        System.out.println("I am a Cetartiodactyla\n"+"---------------------------------\n"+Certa4.Attributes());
         Cetartiodactyla Certa5=new Cetartiodactyla(4, true, true, 1, 5.0, 1289, "Beans and Beans");
-        System.out.println("I am a Cetartiodactyla\n"+Certa5.Attributes());
+        System.out.println("I am a Cetartiodactyla\n"+"---------------------------------\n"+Certa5.Attributes());
 
         Perissodactyla Peris1=new Perissodactyla(4, true, true, 80, 90.0, 1287, "Carrotss");
-        System.out.println("I am a Perissodactyla\n"+Peris1.Attributes());
+        System.out.println("I am a Perissodactyla\n"+"---------------------------------\n"+Peris1.Attributes());
         Perissodactyla Peris2=new Perissodactyla(0, true, true, 0, 0.0, 1500, "Other Animals");
-        System.out.println("I am a Perissodactyla\n"+Peris2.Attributes());
+        System.out.println("I am a Perissodactyla\n"+"---------------------------------\n"+Peris2.Attributes());
         Perissodactyla Peris3=new Perissodactyla(1, true, true, 10, 100, 1568, "Sticks and Dirt");
-        System.out.println("I am a Perissodactyla\n"+Peris3.Attributes());
+        System.out.println("I am a Perissodactyla\n"+"---------------------------------\n"+Peris3.Attributes());
         Perissodactyla Peris4=new Perissodactyla(8, true, true, 78, 90.9, 1567, "Brocolli and Rocks");
-        System.out.println("I am a Perissodactyla\n"+Peris4.Attributes());
+        System.out.println("I am a Perissodactyla\n"+"---------------------------------\n"+Peris4.Attributes());
         Perissodactyla Peris5=new Perissodactyla(4, true, true, 1, 5.0, 1289, "Beans and Beans");
-        System.out.println("I am a Perissodactyla\n"+Peris5.Attributes());
+        System.out.println("I am a Perissodactyla\n"+"---------------------------------\n"+Peris5.Attributes());
 
         Pholidota Pholi1=new Pholidota(4, true, true, 80, 90.0, 1287, "Carrotss");
-        System.out.println("I am a Pholidota\n"+Pholi1.Attributes());
+        System.out.println("I am a Pholidota\n"+"---------------------------------\n"+Pholi1.Attributes());
         Pholidota Pholi2=new Pholidota(0, true, true, 0, 0.0, 1500, "Other Animals");
-        System.out.println("I am a Pholidota\n"+Pholi2.Attributes());
+        System.out.println("I am a Pholidota\n"+"---------------------------------\n"+Pholi2.Attributes());
         Pholidota Pholi3=new Pholidota(1, true, true, 10, 100, 1568, "Sticks and Dirt");
-        System.out.println("I am a Pholidota\n"+Pholi3.Attributes());
+        System.out.println("I am a Pholidota\n"+"---------------------------------\n"+Pholi3.Attributes());
         Pholidota Pholi4=new Pholidota(8, true, true, 78, 90.9, 1567, "Brocolli and Rocks");
-        System.out.println("I am a Pholidota\n"+Pholi4.Attributes());
+        System.out.println("I am a Pholidota\n"+"---------------------------------\n"+Pholi4.Attributes());
         Pholidota Pholi5=new Pholidota(4, true, true, 1, 5.0, 1289, "Beans and Beans");
-        System.out.println("I am a Pholidota\n"+Pholi5.Attributes());
+        System.out.println("I am a Pholidota\n"+"---------------------------------\n"+Pholi5.Attributes());
 
         Carnivora Carni1=new Carnivora(4, true, true, 80, 90.0, 1287, "Carrotss");
-        System.out.println("I am a Carnivora\n"+Carni1.Attributes());
+        System.out.println("I am a Carnivora\n"+"---------------------------------\n"+Carni1.Attributes());
         Carnivora Carni2=new Carnivora(0, true, true, 0, 0.0, 1500, "Other Animals");
-        System.out.println("I am a Carnivora\n"+Carni2.Attributes());
+        System.out.println("I am a Carnivora\n"+"---------------------------------\n"+Carni2.Attributes());
         Carnivora Carni3=new Carnivora(1, true, true, 10, 100, 1568, "Sticks and Dirt");
-        System.out.println("I am a Carnivora\n"+Carni3.Attributes());
+        System.out.println("I am a Carnivora\n"+"---------------------------------\n"+Carni3.Attributes());
         Carnivora Carni4=new Carnivora(8, true, true, 78, 90.9, 1567, "Brocolli and Rocks");
-        System.out.println("I am a Carnivora\n"+Carni4.Attributes());
+        System.out.println("I am a Carnivora\n"+"---------------------------------\n"+Carni4.Attributes());
         Carnivora Carni5=new Carnivora(4, true, true, 1, 5.0, 1289, "Beans and Beans");
-        System.out.println("I am a Carnivora\n"+Carni5.Attributes());
+        System.out.println("I am a Carnivora\n"+"---------------------------------\n"+Carni5.Attributes());
 
     }
 }
