ea270a9a7908cb0b241752725d780dcf583a4ce5
ethancoyle7 <77364000+ethancoyle7@users.noreply.github.com>
2021-10-13 14:38:34 -0500

    suttg

---
+++ PythonPrograms/A02.py
@@ -0,0 +1,25 @@
+# this is the inclass assignment for our python lecture
+# Ethan Coyle
+# Dr. Saikat
+# CMPS 4143- Java/Python
+
+x = 50
+print("c value is :", x)
+if True:
+    print(" i am a true value")
+if (int(x) > 10):
+    print(" that is a non valid value is greater than 10")
+    
+print(" end of our program")
+
+print(" enter a value inside ")
+input=input()
+print(" you have entered", input)
+
+if(int(input)>10):
+    print( "your value geater than 10")
+elif(int(input)==10):
+    print("they are the same")
+else:
+    print(" Smaller than 10")
+
