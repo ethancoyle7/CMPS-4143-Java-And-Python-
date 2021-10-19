a325a4e6671cb43d3456c225f9c63240bf4209a4
Anon-583 <77364000+ethancoyle7@users.noreply.github.com>
2021-10-19 09:42:07 -0500

    Create ProblemOne.py

---
+++ Program3/ProblemOne.py
@@ -0,0 +1,36 @@
+# ====================================================# 
+# Author- Ethan coyle                                 #
+# Inst  - Dr.Saikat                                   #
+# CMPS 4143 Computational Methods (Python and Java)   #
+# ASsignmenr- Problem 2                               #
+# ====================================================#
+
+# create the function definintion to print out hte help
+# head oif the rocket
+def RocketHead():
+  print("   ______ ")
+  print("  *      *")
+  print("  *      *")
+  print("  ********")
+# create the body of the ricket feginition
+def RocketBody():
+  print("  ********")
+  print("  *      *")
+  print("  *      *")
+  print("  ********")
+  print("  *      *")
+  print("  *      *")
+  print("  *      *")
+  print("  ********")
+# create the function definition for the tail of rocket
+def RocketBottom():
+  print("   ______ ")
+  print("  *      *")
+  print(" *        *")
+  print("|****||****|")
+  
+# call the functions and go to their feinition and
+# then will print out rocket
+RocketHead()
+RocketBody()
+RocketBottom()
