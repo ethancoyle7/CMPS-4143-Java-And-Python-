0e84c092502f9783ccbd643d9530219044c9635b
ethancoyle7 <77364000+ethancoyle7@users.noreply.github.com>
2021-10-19 21:44:58 -0500

    tryexcept

--- Program3/ProblemThree.py
+++ Program3/ProblemThree.py
@@ -35,36 +35,34 @@
 
 
 
-
+# creating the definition for myAtoi(passing in the string s inside)
 def myAtoi(s):
     # declare a variable to check the number is positive or negative
-    isPositive = True
+    PositiveNumber = True
 
     # storing the string into in string
-    StringInput = ""
-    
-    # if there nothing in the string return nothing
+    InputValueString = ""
+    # first condition, if there is nothing in the string return
     if len(s)==0:
         return 0
     i=0
     
-    # we need to ignor all the white spaces in the string read in
+    # any white space, we ignore
     while i<len(s):
         if s[i]!=" ":
             break
         i+=1
 
     # if the first character after whitespaces:
-    # 1) is a number -> add it to the StringInput
-    # 2) is "+" -> do nothing
-    # 3) is "-" -> chnage isPositive to False
-    # 4) else return 0
+    # if first char after whitespace is a digit, add to string input
+    # if is a + sign dont do anythin if there is a negative sign
+    # positive value is changed to false. all other cases, return
     if i<len(s):
         if ord('0')<=ord(s[i])<=ord('9'):
-            StringInput += s[i]
-        elif s[i]=="-":
-            isPositive = False
-        elif s[i]=="+":
+            InputValueString += s[i]
+        elif s[i]=="-": # else if as elif
+            PositiveNumber = False # if negative then positive number set to false
+        elif s[i]=="+":# else if as elif
             pass
         else:
             return 0
@@ -74,24 +72,24 @@ def myAtoi(s):
 
     # now run a loop till the end of the string
     while i<len(s):
-        # if the current character is a number, add it to StringInput
+        # if char is a number add to string input
         if ord('0')<=ord(s[i])<=ord('9'):
-            StringInput += s[i]
+            InputValueString += s[i]
         # else, break the loop
         else:
             break
         i+=1
     
     # if the resulting string is empty, return 0
-    if len(StringInput)==0:
+    if len(InputValueString)==0:
         return 0
-    # else check StringInput
+    # else check InputValueString
     else:
-        # if it's true, return minimum of int(StringInput) and 2**31-1
-        if isPositive:
-            return min(int(StringInput),2**31-1)
-        # else, return max of -1*int(StringInput) and -2**31-1
-        return max(-1*int(StringInput), -2**31)
+        # if it's true, return minimum of int(InputValueString) and 2**31-1
+        if PositiveNumber:
+            return min(int(InputValueString),2**31-1)
+        # else, return max of -1*int(InputValueString) and -2**31-1
+        return max(-1*int(InputValueString), -2**31)
 
 
 # main portion of the program where the actual conversions and the
@@ -111,39 +109,74 @@ print("#========================================================#\n",
        "# Assignment- Problem 3                                 #\n",
        "#=======================================================#\n")
 
+try:
+    s="42"
+    myAtoi(s)
+except:
+    print('Cannot convert string to an integer!')
 
-s="42"
-print('Input1:    "',s,'"',"\t\t\t\t\t\t",'OutPut1:    "',myAtoi(s),'"\n') # print out the result of the string read
+print('Input1:    "',s,'"',"\t\t\t\t",'OutPut1:    "',myAtoi(s),'"\n') # print out the result of the string read
 
 # reading in the next string
-s=  "+45"
-print('Input2:    "',s,'"',"\t\t\t\t\t\t",'OutPut2:    "',myAtoi(s),'"\n') # print out the result of the string read
+try:
+    s=  "+45"
+    myAtoi(s)
+except:
+    print('Cannot convert string to an integer!')
+
+print('Input2:    "',s,'"',"\t\t\t\t",'OutPut2:    "',myAtoi(s),'"\n') # print out the result of the string read
 
 # read in the next string
-s= "        -42"
-print('Input3:    "',s,'"',"\t\t\t\t",'OutPut3:    "',myAtoi(s),'"\n') # print out the result of the string read
+try:
+    s= "        -42"
+    myAtoi(s)
+except:
+    print('Cannot convert string to an integer!')
+
+print('Input3:    "',s,'"',"\t\t\t",'OutPut3:    "',myAtoi(s),'"\n') # print out the result of the string read
 
 # read in the string
-s= "4193    with words"
+try:
+
+    s= "4193    with words"   
+    myAtoi(s)
+except:
+    print('Cannot convert string to an integer!')
 print('Input4:    "',s,'"',"\t\t",'OutPut4:    "',myAtoi(s),'"\n') # print out the result of the string read
 
 # read in the string 
-s= "Words with 4856"
+try:
+    s= "Words with 4856"
+    myAtoi(s)
+except:
+    print('Cannot convert string to an integer!')
 print('Input5:    "',s,'"',"\t\t\t",'OutPut5:    "',myAtoi(s),'"\n') # print out the result of the string read
 
 #next string read in
-s= "-91283472332"
+try:
+    s= "-91283472332"
+    myAtoi(s)
+except:
+    print("cannot convert string invalid")
 print('Input6:    "',s,'"',"\t\t\t",'OutPut6:    "',myAtoi(s),'"\n') # print out the result of the string read
 
 # reading in the last string
-s=  "4294967296"
-print('Input7:    "',s,'"',"\t\t\t\t",'OutPut7:    "',myAtoi(s),'"\n') # print out the result of the string read
+try:
+    s=  "4294967296"
+    myAtoi(s)
+except:
+    print("invalid data cannot convert")
+print('Input7:    "',s,'"',"\t\t\t",'OutPut7:    "',myAtoi(s),'"\n') # print out the result of the string read
+
 
 # just for sanity sake, ask the user to input a value read in as a string
 # and see the corresponding output
-
 # ask the user for input string 
-s = str(input("Please enter a string of values\n"))
+try:
+    s = str(input("Please enter a string of values\n"))
+    myAtoi(s)
+except:
+    print("invalid data unable to process")
 print('Input8:    "',s,'"',"\t\t\t\t\t",'OutPut8:    "',myAtoi(s),'"\n') # print out the result of the string read
 
 # end of the program
