1df11646f6ed35f93c89d1d55f929d36ed229722
ethancoyle7 <77364000+ethancoyle7@users.noreply.github.com>
2021-11-03 22:34:21 -0500

    finish problem one

--- Program4/ProblemOne.py
+++ Program4/ProblemOne.py
@@ -23,68 +23,97 @@
 # •	Number of senior students                                            #
 #========================================================================#
 
-# definition main function
-def main():
-    # initialize each variable for reading in
+
+# try opening up the input file specified by the user
+# if acception occured, then print warning and close program
+try:
+    InFile = open('Program4\Students.dat', 'r')
+except:
+    raise OSError("Could not open or read file Exiting Now")
+    
+
+# try opening up the output file, if exception occured then
+# display to viewer
+try:
+    OutFile = open('Program4\student.txt', 'w')
+except:
+    raise OSError("Could not open or write to file Exiting Now")
+
+# if no exceptions occured, continue on with the program
+with InFile:
+    # initialize variables to starting values
     LowestGrade = 0
     HighestGrade = 0
-    total_grades = 0
+    GradeTotal = 0
     NumLines = 0
-    freshman_cnt = 0
-    sophomore_cnt = 0
-    senior_cnt = 0
-    junior_cnt = 0
-    
+    TotFreshMen = 0
+    TotSophmores = 0
+    TotSeniors = 0
+    TotJuniors = 0
     flag = True #flag is true 
     
-    # with the opening of the infile called students.dat
-    with (open('Program4\Students.dat', 'r')) as InFile:
-       
-       # reading each line of the infile
-       for line in InFile: #read line 
-           each_element = line.strip().split(' ') # extract the elements on whitespace
+    # reading each line of the infile
+    for line in InFile: #read line 
+        
+        each_element = line.strip().split(' ') # extract the elements on whitespace
            # after reading each line, the last element is the grade
-           name= str(each_element[1])
-           grade = int(each_element[-1])
-           if flag:
-               LowestGrade = grade
-               HighestGrade = grade
-               flag = False
-           else:
-               if grade < LowestGrade: #if present grade is less than LowestGrade then update 
-             
-                   LowestGrade = grade
-               if grade > HighestGrade: #if present grade is greater than highestGrade then update 
-                   
-                   HighestGrade = grade
-           
-           total_grades = total_grades + grade #find total 
+        # assign default value to start at the first index to hold first and last name
+        Name=str(each_element[0])
+        LName=str(each_element[1])
+        
+        grade = int(each_element[3])
+        if flag:
+           LowestGrade = grade
+           HighestGrade = grade
+           flag = False
+        else:
+           if grade < LowestGrade: #if comparison grade is less  than current, update 
+                LowestGrade = grade
+           if grade > HighestGrade: #if comparison grade is higher than current, update
+                HighestGrade = grade
+                # assign the values of the person with highest grade change the name
+                FirstNamePerson=Name
+                LastNamePerson=LName
+        #find the total grades to be used in average below
+        GradeTotal = GradeTotal + grade 
        
-           NumLines = NumLines + 1 #number of lines
+        NumLines+=1 #increment the line number till no more 
 
-           if each_element[-2].lower() == 'sophomore':
-               sophomore_cnt += 1
-           elif each_element[-2].lower() == 'freshman':
-               freshman_cnt += 1
-           elif each_element[-2].lower() == 'senior':
-               senior_cnt += 1
-           elif each_element[-2].lower() == 'junior':
-               junior_cnt += 1
-              
-    # write to output file 
-    OutFile = open('Program4\student.txt', 'w')
-    # Writing results
-    OutFile.write('Persons Name is :'+ str(name))
-    OutFile.write('\rHighest grade: ' + str(HighestGrade))
-    OutFile.write('\rLowest grade: ' + str(LowestGrade))
-    OutFile.write('\rClass average: %.1f'%(total_grades/NumLines))
-    OutFile.write('\rFreshman: ' + str(freshman_cnt))
-    OutFile.write('\rSophomores: ' + str(sophomore_cnt))
-    OutFile.write('\rJuniors: ' + str(junior_cnt))
-    OutFile.write('\rSeniors: ' + str(senior_cnt) + '\r\n')
-    # Closing file
-    OutFile.close()
-    
+        # iterate over the second index to sum up the class member count for each grade
+        # if occurance occurs, increment the counter  for each
+        if each_element[2].lower() == 'sophomore':
+           TotSophmores += 1
+        elif each_element[2].lower() == 'freshman':
+           TotFreshMen += 1
+        elif each_element[2].lower() == 'senior':
+           TotSeniors += 1
+        elif each_element[2].lower() == 'junior':
+           TotJuniors += 1
+ 
+OutFile.write("After Iterating Through our Infile, We determined that")
+OutFile.write("\r===================================================\n")
+# writing to the output file
+# first we print the person first and last name that had highest grade
+# then we print out the highest grade
+# the display the lowest grade 
+# then the class average  
+OutFile.write('\rPerson With the Highest Grade Was :  '+ FirstNamePerson +" "+LastNamePerson)
+OutFile.write('\rThe Highest Grade Was             :  ' + str(HighestGrade))
+OutFile.write('\rThe Lowest Grade Was              :  ' + str(LowestGrade))
+OutFile.write('\rThe Class Average Was             :  %.1f'%(GradeTotal/NumLines))
 
-#driver code 
-main()
+
+# after we have this printed out, we display the count off all the class 
+# members from freshmen to sophmore and then display the name and then the total for 
+# each class 
+OutFile.write('\r\nThe total # of People in Each Class is Displayed Below\n')
+OutFile.write('=================================================\n')
+OutFile.write('\rTotal # of Freshman   : ' + str(TotFreshMen))
+OutFile.write('\rTotal # of Sophomores : ' + str(TotSophmores))
+OutFile.write('\rTotal # of Juniors    : ' + str(TotJuniors))
+OutFile.write('\rTotal # of Seniors    : ' + str(TotSeniors) + '\r\n')
+
+# we are done with both the input and output so we can close out
+OutFile.close()
+InFile.close()
+
